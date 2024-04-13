7057c25be4e722a6cde5d5f5b724169bda9bef19
rapnyt <rapnyt@gmail.com>
2024-04-13 21:27:09 +0200

    Done some editing and cleaning

--- log_cleaner_scripts/tar_log_create.sh
+++ log_cleaner_scripts/tar_log_create.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-
+#This will move all of the log files to a seperate folder
 n=0
 for file in ~/Scripts/Bash-Scripts/*.log
 do
 	if [ -f "$file" ]
 	then
 		tar -czvf $file.tar.gz $file 2>/dev/null
-		#tar -czvf "~/Scripts/Bash-Scripts/$(basename "$file").tar.gz" "$file"  2>/dev/null
 		n=$(( $n +1 ))
 	fi
 done
