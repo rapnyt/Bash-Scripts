a417a0e44c9cd3b42a1140c882904d3785b8b794
rapnyt <rapnyt@gmail.com>
2024-04-13 12:43:27 +0200

    added some variables with string that are concatenated

--- myscript.sh
+++ myscript.sh
@@ -4,3 +4,20 @@ echo "Hello world!"
 
 echo "My current working directory is: "
 pwd
+
+mynick="Rapnyt"
+myage="40"
+
+echo "Hello, my name is $mynick."
+echo "I'm $myage years old."
+
+#single quotes vs double, need to add escape char in order to continue with the quote if single
+
+echo "I'm a test."
+
+files=$(ls ~)
+
+now=$(date)
+
+echo "The system time and date is: " $now
+
